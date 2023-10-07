10dc89e98628c7ed32e5462ced8c513f4be6e3ff
Angara678 <svainform@gmail.com>
2023-10-07 16:50:48 +0300

    в процессе создания линий для клеток поля

--- Snake v 0_1/Field.py
+++ Snake v 0_1/Field.py
@@ -24,9 +24,17 @@ class Field(DrawnObj.DrawnObj):
 		w = self.block.width - indent*2
 		h = self.block.height - indent*2
 		rect = pygame.Rect(indent, indent, w, h)
-		
+		self.rectField = rect
 		rectColor = (100, 100, 100)
 		pygame.draw.rect(self.block, rectColor, rect, border_radius=20)
+		self.draw_cells()
 		
 		
-
+	def draw_cells(self):
+		rect = self.rectField
+		cellsSize = 50
+		for line in range(self.allCellsInW+1):
+			pygame.draw.line(self.block, (255, 255, 255), (rect.left+5+line*cellsSize, rect.top+5), (rect.right-5+line*cellsSize, rect.bottom-5), 5)
+		
+		for line in range(self.allCellsInH+1):
+			pygame.draw.line(self.block, (0, 255, 255), (rect.top+5+line*cellsSize, rect.left + 5),(rect.bottom-5+line*cellsSize, rect.right - 5), 5)
