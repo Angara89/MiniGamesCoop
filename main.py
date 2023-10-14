afa3de2f15da74454e2f25ef89a0aec946343634
Stradivarius67 <laim6714@mail.ru>
2023-10-14 13:36:17 +0300

    CoreSnake - added lengh tail snake and dinamic food

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import random
 pygame.init()
 
 white = (255, 255, 255)
+yellow = (255, 255, 102)
 blue = (121, 197, 216)
 red = (181, 65, 111)
 black = (0, 0, 0)
-
+green = (0, 255, 0)
 
 disWight = 800
 disHeight = 600
@@ -28,11 +29,16 @@ varY = disHeight / 2
 varXChange = 0
 varYChange = 0
 
-fontStyle = pygame.font.SysFont(None, 50)
+fontStyle = pygame.font.SysFont("bahnschrift", 25)
+scoreFont = pygame.font.SysFont("comicsans", 35)
+
+def ourSnake(snakeblock, snakelist):
+    for x in snakelist:
+        pygame.draw.rect(dis, black, [x[0], x[1], snakeblock, snakeblock])
 
 def message(msg, color):
     mesg = fontStyle.render(msg, True, color)
-    dis.blit(mesg, [disWight/3, disHeight/3])
+    dis.blit(mesg, [disWight/6, disHeight/3])
     
 def gameLoop():
     gameover = False
@@ -43,6 +49,9 @@ def gameLoop():
     
     varxchange = 0
     varychange = 0
+    
+    snakelist = []
+    lengthofsnake = 1
     
     foodx = round(random.randrange(0, disWight - snakeblock) / 10) * 10
     foody = round(random.randrange(0, disWight - snakeblock) / 10) * 10
@@ -88,12 +97,27 @@ def gameLoop():
         dis.fill(white)
         pygame.draw.rect(dis, blue, [foodx, foody, snakeblock, snakeblock])
         pygame.draw.rect(dis, blue, [varx, vary, snakeblock, snakeblock])
+        snakehead = []
+        snakehead.append(varx)
+        snakehead.append(vary)
+        snakelist.append(snakehead)
+        if len(snakelist)> lengthofsnake:
+            del snakelist[0]
+            
+        for x in snakelist[:-1]:
+            if x == snakehead:
+                gameclose = True
+                
+        ourSnake(snakeblock, snakelist)
+        
         pygame.display.update()
         
         clock.tick(snakespeed)
         
         if varx == foodx and vary == foody:
-            print("Ymmy!")
+            foodx = round(random.randrange(0, disWight - snakeblock)/ 10) * 10
+            foody = round(random.randrange(0, disHeight - snakeblock)/ 10) * 10
+            lengthofsnake += 1
         clock.tick(snakespeed)
 
 
