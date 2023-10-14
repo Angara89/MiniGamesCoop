ba65ae8e9bf45ac9af264498584f37d4b2bbdd13
Angara678 <svainform@gmail.com>
2023-10-14 15:43:25 +0300

    проработка класса drawnObj

--- Snake v 0_1/Apple.py
+++ Snake v 0_1/Apple.py
@@ -1,9 +1,11 @@
 import DrawnObj
 import coord
+import block
 
 class Apple(DrawnObj.DrawnObj):
 	
-	def __init__(self):
+	def __init__(self, block: block.Block, tup: tuple, aPoint: tuple):
+		super().__init__(block, tup, aPoint)
 		self.appleCoord = coord.Coord(2,3) # TODO сделать рандомну генерацию координат
 		
 
--- Snake v 0_1/DrawnObj.py
+++ Snake v 0_1/DrawnObj.py
@@ -1,13 +1,15 @@
 import pygame
 from block import Block
 
+
 # TODO придумать как отображать все обьекты данного класса на своем блоке
-class DrawnObj:
+class DrawnObj(pygame.Surface):
 	
-	def __init__(self,  block: Block):
-		self.width = block.width
-		self.height = block.height
-		self.anchorPoint = (0, 0)
+	def __init__(self, block: Block, tup: tuple, aPoint: tuple):
+		super().__init__(tup)
+		self.width = tup[0]
+		self.height = tup[1]
+		self.anchorPoint = aPoint
 		self.myBlock = block
 	
 	def draw(self):
